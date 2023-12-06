7372d422564d997ad1a7bfbd5eedbfe7c6748e00
valmtv <valerij.matviiv@gmail.com>
2023-12-06 20:26:48 +0200

    master - minimal structure & template for C progs

---
+++ c/template.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stlib.h> // for rand() and others
+#include <time.h>  // to set up srand(), use clock() ...
+#include <math.h>  // use math functions: pow(), sqrt(), atan()...
+
+
+int main(void) {
+
+  return 0;
+}
+
